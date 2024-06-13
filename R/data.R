e1ad70b3ef665d03787c1d304b6b740246a115bb
tony2015116 <tony2015116@163.com>
2024-06-13 23:05:34 +0800

    :memo: create mintyr package website

---
+++ R/data.R
@@ -0,0 +1,35 @@
+#' @title nedap
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 31863 rows and 9 variables:
+#' \describe{
+#'   \item{\code{animal_number}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{lifenumber}}{logical COLUMN_DESCRIPTION}
+#'   \item{\code{responder}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{location}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{visit_time}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{duration}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{state}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{weight}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{feed_intake}}{integer COLUMN_DESCRIPTION}
+#'}
+#' @source \url{http://somewhere.important.com/}
+"nedap"
+
+
+#' @title fire
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 9794 rows and 10 variables:
+#' \describe{
+#'   \item{\code{Location}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{Tag}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{Date}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{Entry}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{Exit}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{Ent Wt}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{Ext Wt}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{Consumed}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{Weight}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{Topup Amount}}{double COLUMN_DESCRIPTION}
+#'}
+#' @source \url{http://somewhere.important.com/}
+"fire"

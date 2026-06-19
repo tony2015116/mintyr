3470724e27aa9e16dba396174fd2f5608a83f20c
tony2015116 <tony2015116@163.com>
2026-06-19 17:50:49 +0800

    :memo: update mintyr

--- R/zzz.R
+++ R/zzz.R
@@ -1,30 +1,19 @@
 utils::globalVariables("..non_nested_cols")
 
 .onAttach <- function(libname, pkgname) {
-  pkg_version <- as.character(utils::packageVersion("mintyr"))
+  v <- as.character(utils::packageVersion("mintyr"))
 
-  # 动态时间问候 (使用 ASCII Unicode 转义)
-  # \U0001F989 (🦉), \u2615 (☕), \u2600 (☀️), \U0001F319 (🌙)
   hour <- as.numeric(format(Sys.time(), "%H"))
-  greeting <- if (hour < 5) {
-    "\U0001F989 Late night hacking? Remember to save your workspace."
-  } else if (hour < 12) {
-    "\u2615 Good morning! Coffee first, data second."
-  } else if (hour < 18) {
-    "\u2600 Good afternoon! Keep calm and analyze on."
-  } else {
-    "\U0001F319 Good evening! May your code run fast while you sleep."
-  }
+  greeting <- if (hour < 5)
+    "\U0001F33F Late night hacking? Remember to save your workspace."
+  else if (hour < 12)
+    "\U0001F33F Good morning! Coffee first, data second."
+  else if (hour < 18)
+    "\U0001F33F Good afternoon! Keep calm and analyze on."
+  else
+    "\U0001F33F Good evening! May your code run fast while you sleep."
 
-  # 组装启动信息
-  # \U0001F33F (🌿)
-  msg <- paste0(
-    "\n",
-    "==========================================================\n",
-    " \U0001F33F mintyr v", pkg_version, " Loaded.\n",
-    " ", greeting, "\n",
-    "==========================================================\n"
-  )
-
-  packageStartupMessage(msg)
+  packageStartupMessage(paste0(
+    greeting
+  ))
 }
